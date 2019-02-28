fa156be7a82b51d6b75bfdfd2b7b5f32a16c291a
unknown <developer@addoeventi.it>
2019-02-28 11:43:13 +0100

    automatic commit

--- lib/warehouse.model.d.ts
+++ lib/warehouse.model.d.ts
@@ -2,9 +2,12 @@ import { Product } from './product.model';
 import { Discharge } from './discharge.model';
 import { ShoppingList } from './shopping-list.model';
 import { Outgo } from "./outgo.model";
+import { Barman } from "./barman.model";
 export declare class Warehouse {
     guid: string;
     name: string;
+    code: string;
+    barmanCodes: Barman[];
     products: Product[];
     discharges: Discharge[];
     shoppingList: ShoppingList;
--- models/warehouse.model.ts
+++ models/warehouse.model.ts
@@ -2,13 +2,16 @@ import { Product } from './product.model';
 import { Discharge } from './discharge.model';
 import { ShoppingList } from './shopping-list.model';
 import { Outgo } from "./outgo.model";
+import { Barman } from "./barman.model";
 
 export class Warehouse {
     public guid: string;
     public name: string;
+    public code: string;
+    public barmanCodes: Barman[];
     public products: Product[] = [];
     public discharges: Discharge[] = [];
     public shoppingList: ShoppingList;
-    public history: Outgo[] = []
+    public history: Outgo[] = [];
     public modified: Date;
 }
